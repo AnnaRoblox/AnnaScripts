7aecea8857b7b45ac1d6a6e89e2c11ec185655c4
Anna <annathegoodgirl@protonmail.com>
2025-02-02 14:24:04 -0600

    Create cant say the word get among us tool.lua

---
+++ cant say the word get among us tool.lua
@@ -0,0 +1,26 @@
+--normal: 
+game:GetService("Players").LocalPlayer.PlayerGui.Main.Frame.Amogus.Amogus.LocalScript.RemoteEvent1:FireServer()
+--iy plugin (removes the -'s )
+--local Plugin = {
+	--["PluginName"] = "AmogusPlugin",
+	--["PluginDescription"] = "Fires the Amogus remote event multiple times",
+	--["Commands"] = {
+		--["getamong"] = {
+		--	["ListName"] = "getamong",
+		--	["Description"] = "Fires the Amogus remote event a specified number of times",
+		--	["Aliases"] = {"amogus", "sus"},
+		--	["Function"] = function(args, speaker)
+			--	local count = tonumber(args[1]) or 1
+			--	for i = 1, count do
+				--	game:GetService("Players").LocalPlayer.PlayerGui.Main.Frame.Amogus.Amogus.LocalScript.RemoteEvent1:FireServer()
+			--	end
+			-- end,
+	--	},
+	-- },
+-- }
+
+
+
+
+
+-- return Plugin
